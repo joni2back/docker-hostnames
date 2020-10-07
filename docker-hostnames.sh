bfe19d927ebb47eb9e85ac39e05e2217973469bb
Juan Eugenio Abadie <juaneabadie@gmail.com>
2020-10-07 08:44:48 -0300

    Check if there are running containers to avoid inspect error

--- docker-hostnames.sh
+++ docker-hostnames.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
-sed -i '/\#dockerhostnames/d' /etc/hosts && echo "$(docker ps -q | xargs -n 1 docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Name }} #dockerhostnames' | sed 's/ \// /')" | tee -a /etc/hosts
+sed -i '/\#dockerhostnames/d' /etc/hosts &&
+  [ "$(docker ps -q)" ] &&
+  docker ps -q |
+    xargs -n 1 docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Name }} #dockerhostnames' |
+    sed 's/ \// /' |
+    tee -a /etc/hosts
